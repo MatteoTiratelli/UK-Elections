b40e364b52955f4412cbfa132475619c311f4d23
MatteoTiratelli <69040868+MatteoTiratelli@users.noreply.github.com>
2022-01-04 14:33:35 +0000

    Update Graphs.r

--- Graphs.r
+++ Graphs.r
@@ -82,7 +82,7 @@ ggplot(drop_na(data[data$variable %in% c('MPs_Business','MPs_Political','MPs_Pro
   scale_y_continuous(limits = c(0,100)) +
   scale_colour_manual(values = c('MPs_Business' = 'blue','MPs_Political' = 'purple',
                                  'MPs_Professionals' = 'green','MPs_Manual' = 'red'),
-                        name = NULL, labels = c("Business", "Manual labour", "Political work", "Professional")) +
+                      name = NULL, labels = c("Business", "Manual labour", "Political work", "Professional")) +
   theme_base('sans') +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
         legend.position = c(.95, .95),
@@ -126,3 +126,14 @@ ggplot(data[data$variable %in% c('Lab_seats','Lab_vote'),], aes(x = Year, y = va
         legend.margin = margin(6, 6, 6, 6),
         legend.direction = "horizontal",
         legend.box.background = element_rect(colour = "black"))
+
+data[!(data$Year %in% c('1918','1922','1923','1924','1929','1931','1935')),] -> data1
+
+ggplot(data = data1[data1$variable %in% c('Lab_seats_raw', "Lab_vote"),], aes(x = Year, y = value)) +
+  geom_col(fill = 'grey80') +
+  xlab(NULL) + ylab(NULL) +
+  labs(title = "The Labour Party's Electoral Performance", caption = "Source: House of Commons Library") +
+  theme_base('sans') +
+  facet_grid(rows = vars(variable), scales = "free", space = "fixed") +
+  geom_col(data = data1[data1$variable %in% c('Lab_seats_raw', "Lab_vote") & data1$Year == "2019",], aes(x = Year, y = value), fill = 'red') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
